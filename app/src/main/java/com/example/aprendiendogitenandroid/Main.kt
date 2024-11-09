4fbc594de22cedd59e6d575c3be228c6c80f9996
JonnyIvan24 <johnny.perez.u@gmail.com>
2024-11-09 12:35:51 -0600

    Se creo el flujo de login

--- app/src/main/java/com/example/aprendiendogitenandroid/Main.kt
+++ app/src/main/java/com/example/aprendiendogitenandroid/Main.kt
@@ -3,8 +3,13 @@ package com.example.aprendiendogitenandroid
 fun main() {
     println("Hola")
     listadoDeUsuarios()
+    login("email@test.com", "hola1234")
 }
 
 fun listadoDeUsuarios() {
     // return Usuarios[]
+}
+
+fun login(email: String, password: String) {
+    // petición de API para hacer login
 }
